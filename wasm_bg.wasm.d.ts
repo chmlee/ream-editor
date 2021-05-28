8bb31e6e054649e25aeccfaadcc5f87881acfc5d
Chih-Ming Louis Lee <louis@chihminglee.com>
2021-05-28 15:15:34 -0500

    update ream-core to v0.4.1

--- wasm_bg.wasm.d.ts
+++ wasm_bg.wasm.d.ts
@@ -7,7 +7,7 @@ export function ream2csv(a: number, b: number, c: number): void;
 export function __wbindgen_malloc(a: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number): number;
 export const __wbindgen_export_2: WebAssembly.Table;
-export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h6c3e3e322876a71d(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h02e13e1bc4183c8d(a: number, b: number, c: number): void;
 export function __wbindgen_add_to_stack_pointer(a: number): number;
 export function __wbindgen_free(a: number, b: number): void;
 export function __wbindgen_exn_store(a: number): void;
